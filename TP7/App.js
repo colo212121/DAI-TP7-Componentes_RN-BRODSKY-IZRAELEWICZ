ccc527d3d551171f05ed59f7025c91c323883127
Elias Brodsky <111788044+colo212121@users.noreply.github.com>
2025-05-12 12:14:21 -0300

    casi terminado

--- TP7/App.js
+++ TP7/App.js
@@ -9,47 +9,62 @@ import {
   View,
   TouchableOpacity,
   Alert,
-  Pressable
+  Pressable,
+  Image
 } from 'react-native';
 
 const imagenFondo = {
   uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAvDXLGQKpRM8mMKJzme4NftrhRck1ZzDA_g&s'
 };
 
+const imagenPerfil = {
+  uri: 'https://media.cnn.com/api/v1/images/stellar/prod/cnne-407307-160927210830-tk-ah0927-exlarge-169.jpg?c=16x9&q=h_833,w_1480,c_fill'
+}
+
 export default function App() {
   const [mensaje, handleMensajeChange] = useState('');
-
+  const [Fondo, setFondo] = useState(false);  
+  
   const handlePress = () => {
     Alert.alert('Mensaje enviado', mensaje || 'No se ingresó ningún mensaje');
   };
-  
-  const [fondoAzul, setFondoAzul] = useState(false);
+
+  const handlePress2 = () => {
+    setFondo(!Fondo);  
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground
-        source={fondoAzul ? null : imagenFondo}
-        style={[styles.backgroundImage, { backgroundColor: fondoAzul ? 'blue' : 'transparent' }]}
-        blurRadius={fondoAzul ? 0 : 3}
-      >
-        <View style={styles.overlay}>
-          <Text style={styles.name}>Pepe</Text>
-          <Text style={styles.subtitle}>Backend Developer</Text>
+        source={imagenFondo}
+        style={styles.backgroundImage}>
+        <View style={[styles.container, { backgroundColor: Fondo ? 'gray' : 'grey' }]}>
+        <Image source={imagenPerfil} style={styles.Perfil} />
+
+          <Text style={styles.nombre}>Pepe</Text>
+          <Text style={styles.titulo}>Backend Developer</Text>
+          
           <TextInput
             style={styles.input}
             placeholder="Enter a message"
-            placeholderTextColor="#ccc"
+            placeholderTextColor="#aaa"
             onChangeText={handleMensajeChange}
             value={mensaje}
           />
+          
           <TouchableOpacity style={styles.button} onPress={handlePress}>
-            <Text style={styles.buttonText}>Contactar</Text>
+            <Text style={styles.textoButton}>Contactar</Text>
           </TouchableOpacity>
+          
           <Pressable
-            style={[styles.button, { backgroundColor: 'blue', marginTop: 10 }]}
-            onPress={() => setFondoAzul(true)}
-          >
-            <Text style={styles.buttonText}>Ver Perfil</Text>
+            onPress={handlePress2}
+            style={({ pressed }) => [
+              styles.button,{backgroundColor: pressed ? '#4e555b' : '#6C757D', },]}>
+            <Text style={styles.textoButton}>
+              {Fondo ? "Perfil Activo" : "Ver Perfil"}
+            </Text>
           </Pressable>
+
           <StatusBar style="light" />
         </View>
       </ImageBackground>
@@ -61,50 +76,60 @@ const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-    padding: 30,
-    borderRadius: 20,
-    width: '80%',
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 5 },
-    shadowOpacity: 0.3,
-    shadowRadius: 6,
-    elevation: 8
   },
-  name: {
-    fontSize: 26,
+
+  Perfil: {
+    width: 100,   
+    height: 100, 
+    borderRadius: 50,  
+    marginBottom: 20,
+  },
+
+  container: {
+    flex: 0.5,                   
+    justifyContent: 'center',    
+    alignItems: 'center',         
+    padding: 20,                  
+    borderRadius: 10,                
+  },
+
+  nombre: {
+    fontSize: 28,
     fontWeight: 'bold',
-    color: 'white',
-    marginBottom: 8
+    color: '#FFFFFF',
+    marginBottom: 8,
   },
-  subtitle: {
-    fontSize: 18,
-    color: '#ddd',
-    marginBottom: 20
+
+  titulo: {
+    fontSize: 20,
+    color: '#DCDCDC',  
+    marginBottom: 20,
   },
+
   input: {
-    height: 45,
+    height: 60,
     width: '100%',
-    backgroundColor: 'white',
-    borderRadius: 10,
+    backgroundColor: '#F0F0F0',  
+    borderRadius: 8,
     paddingHorizontal: 15,
     fontSize: 16,
-    color: '#333',
-    marginBottom: 15
+    color: '#333', 
+    marginBottom: 15,
   },
+
   button: {
-    backgroundColor: '#00aced',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10
+    backgroundColor: '#6C757D',
+    paddingVertical: 12,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    marginBottom: 10,
   },
-  buttonText: {
+
+  textoButton: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 16
-  }
+    fontSize: 16,
+    textAlign: 'center',
+  },
 });
